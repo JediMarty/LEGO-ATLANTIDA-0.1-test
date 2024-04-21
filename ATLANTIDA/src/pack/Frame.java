1f4a6c66085d0c100d56fa6f5e547d1b3a58ceb4
JediMarty <130549056+JediMarty@users.noreply.github.com>
2024-04-21 15:12:10 +0300

    Update Frame.java

--- ATLANTIDA/src/pack/Frame.java
+++ ATLANTIDA/src/pack/Frame.java
@@ -1,19 +1,37 @@
 package pack;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+
 
 public class Frame extends JFrame{
 
+	
 	public Frame() {
 		init();
 	}
 	
 	public void init() {
 		
+        JLabel label = new JLabel();
+	
+		ImageIcon bg = new ImageIcon("darkrain.jpg");
+		
+     
+		label.setIcon(bg);
+		
+		
+        
+		//The Window
 		this.setTitle("ATLANTIDA 0.1-test");
 		this.setSize(800,800);
 		this.setResizable(false);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		this.add(label);
+	   
 		this.setVisible(true);
+		
 	}
 }
