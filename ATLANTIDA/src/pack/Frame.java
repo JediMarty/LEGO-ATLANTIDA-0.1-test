7729b27f2e8c253eae776638958b4a56c1e7f73e
JediMarty <130549056+JediMarty@users.noreply.github.com>
2024-04-27 20:58:04 +0300

    Update Frame.java

--- ATLANTIDA/src/pack/Frame.java
+++ ATLANTIDA/src/pack/Frame.java
@@ -2,12 +2,14 @@ package pack;
 
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.Image;
 import java.awt.LayoutManager;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.OverlayLayout;
+import javax.swing.SwingConstants;
 
 public class Frame extends JFrame{
 
@@ -21,33 +23,36 @@ public class Frame extends JFrame{
 		int num_test = 1000;
 		JLabel label = new JLabel();
 		JLabel Count_label = new JLabel();
+		JLabel baseimg = new JLabel();
 		ImageIcon bg = new ImageIcon("darkrain.jpg");
-		JPanel panel = new JPanel();
-     
-		label.setIcon(bg);
-		
+		ImageIcon base = new ImageIcon("Abase.png");
+
+	    label.setIcon(bg);
+	    label.setBounds(0,0,800,800);
+	    
 		Count_label.setText(String.valueOf(num_test));
 	    Count_label.setForeground(Color.WHITE);
 	    Count_label.setFont(new Font("MV Boli",Font.BOLD,30));
-		
-		LayoutManager overlay = new OverlayLayout(panel);
-        panel.setLayout(overlay);
+	    Count_label.setVerticalAlignment(JLabel.TOP);
+	    Count_label.setHorizontalAlignment(JLabel.RIGHT);
+		Count_label.setBounds(0,0,780,780);
+
+	    Image modifiedbaseimg = base.getImage().getScaledInstance(450, 400, java.awt.Image.SCALE_SMOOTH);
+	    base = new ImageIcon(modifiedbaseimg);
+	    baseimg.setIcon(base);
+	    baseimg.setBounds(0,150,500,500);
         
-        panel.add(Count_label);
-        Count_label.setAlignmentX(RIGHT_ALIGNMENT);
-        Count_label.setAlignmentY(TOP_ALIGNMENT);
-        
-        panel.add(label);
-        label.setAlignmentX(RIGHT_ALIGNMENT);
-        label.setAlignmentY(TOP_ALIGNMENT);
-		
-        //The Window
+	    //The Window
 		this.setTitle("ATLANTIDA 0.1-test");
 		this.setSize(800,800);
 		this.setResizable(false);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.add(panel);
+		this.add(Count_label);
+        this.add(baseimg);
+        this.add(label);
+	    this.setLayout(null);
 	    this.setVisible(true);
 		
 	}
 }
+
