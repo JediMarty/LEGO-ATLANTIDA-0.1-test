bab3ea4c0b86680986af2a1acbc257614ae96736
JediMarty <130549056+JediMarty@users.noreply.github.com>
2024-05-23 13:58:36 +0300

    Update Menu.java

--- ATLANTIDA/src/pack/Menu.java
+++ ATLANTIDA/src/pack/Menu.java
@@ -1,14 +1,19 @@
 package pack;
 
+import java.awt.Cursor;
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
-public class Menu {
+public class Menu implements ActionListener{
 
+	public static JFrame framemenu = new JFrame();
+	public static int x = 0;
 	JButton PlayButton = new JButton();
 	
 	public Menu() {
@@ -26,10 +31,10 @@ public class Menu {
 		PlayButton.setFocusable(false);
 		PlayButton.setFont(new Font("MV Boli",Font.BOLD,30));
 		PlayButton.setBounds(260, 250, 300, 100);
+		PlayButton.addActionListener(this);
+		PlayButton.setCursor(new Cursor(Cursor.HAND_CURSOR));
 		
-		
-		JFrame framemenu = new JFrame();
-		framemenu.setTitle("ATLANTIDA 0.1-test");
+	    framemenu.setTitle("ATLANTIDA 0.1-test");
 		framemenu.setSize(800,800);
 		framemenu.setResizable(false);
 		framemenu.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);   
@@ -37,6 +42,16 @@ public class Menu {
 		framemenu.add(labelbg);
 		framemenu.setLayout(null);
 		framemenu.setVisible(true);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		
+		if (e.getSource() == PlayButton) {
+			x = 1;
+			
+		}
+		
 	}
 	
 }
