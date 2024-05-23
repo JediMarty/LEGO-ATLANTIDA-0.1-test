ec70148f2f26537d542359f527a8a03b03491dcf
JediMarty <130549056+JediMarty@users.noreply.github.com>
2024-05-23 13:58:54 +0300

    Update Main.java

--- ATLANTIDA/src/pack/Main.java
+++ ATLANTIDA/src/pack/Main.java
@@ -3,12 +3,13 @@ package pack;
 public class Main{
 
 	public static void main(String[] args) {
+		while(Menu.x != 1) {
 		new Menu();		
-		
-		if(Menu.x == 1) {
+		}
+
 	    Menu.framemenu.dispose();
 		new Frame();
-		}
+		
 	}
 
 }
