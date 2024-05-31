96719583a542bd46c024b2ff45756bccf763d42e
JediMarty <130549056+JediMarty@users.noreply.github.com>
2024-05-31 18:03:32 +0300

    Update explore.java

--- ATLANTIDA/src/pack/explore.java
+++ ATLANTIDA/src/pack/explore.java
@@ -1,19 +1,40 @@
 package pack;
 
 
+import java.awt.Color;
+
+import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 
 public class explore {
 	
+	public static JLabel bg;
+	public static JLabel unitlb;
+    public static JLabel monsterlb;	
 
-	public static JLabel lb = new JLabel("ola!");
 	public explore() {
 		init2();
 	}
 	
 	public void init2() {
 		
-		lb = new JLabel("hi/test!");
+		ImageIcon img1 = new ImageIcon("unit1.png");
+		ImageIcon img2 = new ImageIcon("monster1.png");
+		
+		bg = new JLabel();
+		bg.setIcon(Frame.bg);
+		
+		unitlb = new JLabel();
+		unitlb.setIcon(img1);
+		unitlb.setBackground(Color.BLUE);
+		unitlb.setOpaque(true);
+		
+		monsterlb = new JLabel();
+		monsterlb.setIcon(img2);
+		monsterlb.setBackground(Color.DARK_GRAY);
+		monsterlb.setOpaque(true);
+
 	}
 
 }
+
