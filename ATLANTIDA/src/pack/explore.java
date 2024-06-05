d3a800bacfa0fa0847732fe70b8e70d5b4f3d3da
JediMarty <130549056+JediMarty@users.noreply.github.com>
2024-06-05 18:40:38 +0300

    Update explore.java

--- ATLANTIDA/src/pack/explore.java
+++ ATLANTIDA/src/pack/explore.java
@@ -13,7 +13,7 @@ public class explore {
 	public static JLabel bg;
 	public static JLabel unitlb;
     public static JLabel monsterlb;	
-
+    static ImageIcon img2 = new ImageIcon("monster1.png");
 	public explore() {
 		init2();
 		
@@ -22,7 +22,7 @@ public class explore {
 	public void init2() {
 		
 		ImageIcon img1 = new ImageIcon("unit1.png");
-		ImageIcon img2 = new ImageIcon("monster1.png");
+		
 		
 		bg = new JLabel();
 		bg.setIcon(Frame.bg);
@@ -33,23 +33,33 @@ public class explore {
 		unitlb.setOpaque(true);
 		
 		monsterlb = new JLabel();
-		monsterlb.setIcon(img2);
+		monsterlb.setIcon(null);
 		monsterlb.setBackground(Color.DARK_GRAY);
 		monsterlb.setOpaque(true);
 
 	}
 	
-public static void action() {
+	public static void action() {
+		//this method performs the different situations in the game!
+		int num1 = (int) (Math.random() * 9 + 1);
+		int num2 = (int) (Math.random() * 9 + 1);
 		
-		int num = (int) (Math.random() * 9 + 1);
-		
-		if(num >5) {
-			showMessageDialog(null, "You win");
+		if(num1 == 5 || num1 == 6) {
+			monsterlb.setIcon(img2);
+			if(num2 >5) {
+				showMessageDialog(null, "You destroy the sea monster, congratulations!");
+			}
+			
+			else {
+			showMessageDialog(null, "The sea monster destroy you, you lost!");
+			}
+			
 		}
 		
 		else {
-			showMessageDialog(null, "You lost");
+			showMessageDialog(null, "other, not completed yet....");
 		}
 	}
 
 }
+
